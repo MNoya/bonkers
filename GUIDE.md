1f5512760fe60726c5c64c5a68c92dae3f37eecd
Martin Noya <martinnoya@gmail.com>
2016-10-31 09:13:17 -0300

    Updated Guide, added drums

--- GUIDE.md
+++ GUIDE.md
@@ -1,22 +1,31 @@
 How to Big Red Button on Ubuntu 16.04
 
 1. Download & unzip *bonkers*
+
+```
     cd ~
     curl -LOk https://github.com/MNoya/bonkers/archive/master.zip
     unzip master.zip
     mv bonkers-master bonkers
+```
 
 2. Install required stuff
+
+```
     sudo apt install -y libusb-1.0-0 libusb-1.0-0-dev sox libsox-fmt-mp3
+```
 
 3. Permissions
+
+```
     cd bonkers
     sudo cp doc/99-bonkers.rules /etc/udev/rules.d
-    udevadm control --reload-rules
+    sudo udevadm control --reload-rules
+```
 
 4. Compile and run
-    make
-    ./home/$USER/bonkers/bonkers -q -c "bash /home/$USER/bonkers/play.sh"
 
-5. (Optional) Add it as an autorun script
-    ![img](http://imgur.com/vLB3pnE)
+```
+    make
+    bonkers -q -c "bash play.sh"
+```
--- play.sh
+++ play.sh
@@ -8,43 +8,36 @@ LID_PREV=$4
 case "$BUTTON_NOW $LID_NOW $BUTTON_PREV $LID_PREV" in
     "0 0"*)
         # Button not being pressed, lid shut
-        echo "Closed"
         if pgrep play; then pkill play; fi
         ;;
 
     "1 0"*)
         # Button being pressed, lid shut?  Weird.
-        echo "Odd - cover is shut and you are pressing the button"
         ;;
 
     "1 1"*)
         # Button being pressed, lid open
         if pgrep play; then pkill play; fi
-        echo "Playing sound"
         play -q ~/bonkers/sound.mp3 &
         ;;
 
     "0 1 0 0")
         # Button not pressed, lid open
         # Previously button was not pressed, lid was closed
-        echo "Open"
+        if pgrep play; then pkill play; fi
+        play -q ~/bonkers/drums.mp3 &
         ;;
 
     "0 1 1 0")
         # Button not pressed, lid open
         # Previously button was pressed, lid was closed
-        echo "Extremely odd that you changed both things at once"
         ;;
 
     "0 1 1 1")
         # Button not pressed, lid open
         # Previously button was pressed, lid was open
-        echo "Release"
         ;;
 
     *)
-        echo -n "Strange arguments:"
-        printf " %q" "$@"
-        echo ""
         ;;
 esac
